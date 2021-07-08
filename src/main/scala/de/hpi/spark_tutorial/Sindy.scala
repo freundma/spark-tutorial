6d9466c3fbcc3063857b00fdb1a6b5c03a4fa37d
freundma <mariofreund2@gmail.com>
2021-07-08 19:38:00 +0200

    Changes by Felix

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -15,20 +15,44 @@ object Sindy {
         .csv(path = path)
     }
 
-    val region = csv_to_rdd(inputs.head)
-    val nation = csv_to_rdd(inputs(1))
-    val supplier = csv_to_rdd(inputs(2))
 
-    val regionFlat = region.as[(String, String, String)].
-      flatMap(f => f.productIterator.asInstanceOf[Iterator[String]].toList zip region.schema.fieldNames)
+    val region = csv_to_df(inputs.head)
+    val nation = csv_to_df(inputs(1))
+    val supplier = csv_to_df(inputs(2))
+    val customer= csv_to_df(inputs(3))
+    val part = csv_to_df(inputs(4))
+    val lineitem = csv_to_df(inputs(5))
+    val orders = csv_to_df(inputs(6))
+
+    val regionFieldNames = region.schema.fieldNames
+    val nationFieldNames = nation.schema.fieldNames
+    val supplierFieldNames = supplier.schema.fieldNames
+    val customerFieldNames = customer.schema.fieldNames
+    val partFieldNames = part.schema.fieldNames
+    val lineitemFieldNames = lineitem.schema.fieldNames
+    val ordersFieldNames = orders.schema.fieldNames
+
+    val regionFlat = region.as[(String, String, String)]
+      .flatMap(f => List(f._1, f._2, f._3) zip regionFieldNames)
 
     val nationFlat = nation.as[(String, String, String, String)]
-      .flatMap(f => f.productIterator.asInstanceOf[Iterator[String]].toList zip nation.schema.fieldNames)
+      .flatMap(f => List(f._1, f._2, f._3, f._4) zip nationFieldNames)
 
     val supplierFlat = supplier.as[(String, String, String, String, String, String, String)]
-      .flatMap(f => f.productIterator.asInstanceOf[Iterator[String]].toList zip supplier.schema.fieldNames)
+      .flatMap(f => List(f._1, f._2, f._3, f._4, f._5, f._6, f._7) zip supplierFieldNames)
 
+    val customerFlat = customer.as[(String, String, String, String, String, String, String, String)]
+      .flatMap(f => List(f._1, f._2, f._3, f._4, f._5, f._6, f._7, f._8) zip customerFieldNames)
+
+    val partFlat = part.as[(String, String, String, String, String, String, String, String, String)]
+      .flatMap(f => List(f._1, f._2, f._3, f._4, f._5, f._6, f._7, f._8, f._9) zip partFieldNames)
     nationFlat.show()
+
+    val lineitemsFlat = lineitem.as[(String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String)]
+      .flatMap(f => List(f._1, f._2, f._3, f._4, f._5, f._6, f._7, f._8, f._9, f._10, f._11, f._12, f._13, f._14, f._15, f._16) zip lineitemFieldNames)
+
+    val ordersFlat = orders.as[(String, String, String, String, String, String, String, String, String)]
+      .flatMap(f => List(f._1, f._2, f._3, f._4, f._5, f._6, f._7, f._8, f._9) zip ordersFieldNames)
 
     val attributeValuePairs = regionFlat.union(nationFlat).union(supplierFlat)
 
@@ -44,17 +68,5 @@ object Sindy {
 
     reducedInclusionList.foreach(f => println(f._1 + " < " + f._2.toArray.mkString(", ")))
 
-
-    /*val tables = spark
-      .read
-      .option("inferSchema", "false")
-      .option("header", "true")
-      .option("delimiter", ";")
-      .csv(inputs: _*)
-
-    tables.show(numRows = 30)
-    tables.flatMap()*/
-
-    // TODO
   }
 }
